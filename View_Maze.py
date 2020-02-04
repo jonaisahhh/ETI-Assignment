bccbc3867c9e020e11c4bc8a7c2be17910b12ded
Arm57ar <34701555+Arm57ar@users.noreply.github.com>
2020-02-05 04:51:34 +0800

    View Maze updated

---
+++ View_Maze.py
@@ -0,0 +1,17 @@
+import sys
+import csv
+import time
+
+def read_csv():
+    print(fileoutput)
+
+    with open("D:\NgeeAnn\Year3_Sems_2\ETI_Module\Python\View_Maze.py", 'rt') as csvfile:
+
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            #print(', '.join(row))
+            print(row)
+    #menu()
+    read_csv()
+
+
