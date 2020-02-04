ec46e7458dc1eef74919c98f6795cfd2014bf766
Reynaldi <epicboyyoumad0@gmail.com>
2020-02-05 01:06:25 +0800

    Integration of read_maze.csv

--- maze_game.py
+++ maze_game.py
@@ -18,6 +18,7 @@ def menu():
 """)
     choice = input("Please enter your choice:")
     if choice == "1" or choice =="1":
+        load_csv()
         return("To be implemented1")
     elif choice == "2" or choice =="2":
         return("To be implemented2")
@@ -32,6 +33,29 @@ def menu():
         print("Please try again")
         
 
+def load_csv():
+    file = input("Enter the name of data file:")
 
+    #Try Catch Block
+    try:
+        with open(file,'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            row_count=sum(1 for row in csvfile)
+            print("Reading",file, "...")
+            print("Number of lines read:", row_count)
+            if len(fileoutput) >= 1:
+                fileoutput.clear()
+                fileoutput.append(file)
+                return 1
+            else:
+                fileoutput.append(file)
+                return 1
+            #Return back to menu 
+            menu()
+    except FileNotFoundError:
+        print(file,"does not exist. Try again")
+        return 0
+        load_csv()
+        
 
 menu()
