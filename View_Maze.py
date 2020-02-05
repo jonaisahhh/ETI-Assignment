0e5833f1827f84278f57551111c12eb044d14142
Arm57ar <34701555+Arm57ar@users.noreply.github.com>
2020-02-05 20:29:04 +0800

    Updated_View_Maze

--- View_Maze.py
+++ View_Maze.py
@@ -3,15 +3,16 @@ import csv
 import time
 
 def read_csv():
-    print(fileoutput)
+    #print(fileoutput)
 
-    with open("fileoutput[0]", 'rt') as csvfile:
+    file_directory = "D:\\NgeeAnn\\Year3_Sems_2\\ETI_Module\\Python\\maze.csv"
+    with open(file_directory,'rt') as csvfile:
 
         spamreader = csv.reader(csvfile)
         for row in spamreader:
             #print(', '.join(row))
             print(row)
     #menu()
-    read_csv()
+read_csv()
 
 
