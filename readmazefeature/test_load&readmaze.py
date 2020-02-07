bdb97a6bf5c86906b310f91081a1432a2ad76aff
Jianwen Rayner Tan <s10177641@connect.np.edu.sg>
2020-02-07 08:10:25 +0800

    Passed feature 2 unit test cases

--- readmazefeature/test_load&readmaze.py
+++ readmazefeature/test_load&readmaze.py
@@ -1,11 +1,8 @@
 #Load maze game from csv
 import os
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from readmaze import *
+
 # Read and load maza from file, this feature allows the application to read the maza diagram from the data file and store it in memory. It also stores the start (A) location and
 # the end (B) location. The application should prompt the user for the name of the data file. The number of records/lines read is then displayed
 
@@ -15,7 +12,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_CorrectInput():
     trytoload = load_csv("maze.csv")
     #Call the function to load csv, expected return type should be a 1, 1 for success, 0 for fail
-    assert trytoload == 1
+    assert trytoload == "Success"
 
 #2 Failing Test Case
 
@@ -23,10 +20,10 @@ def test_CorrectInput():
 def test_WrongName():
     trytoload = load_csv('msze.csv')
     #Call function to load csv with filenameToLoad
-    assert trytoload == 0 #
+    assert trytoload == "File not found" #
 
 #User input the wrong extension
 def test_WrongExt():
     trytoload = load_csv('maze.doc')
     #Call function to load csv with filenameToLoad
-    assert trytoload == 0 # Expect 0 failing
+    assert trytoload == "File not found" # Expect 0 failing
