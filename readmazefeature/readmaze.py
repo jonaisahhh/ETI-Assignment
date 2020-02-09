89740b364da0b2de742b66b30a1c14d60252911a
Rayner <44660318+kazooie20@users.noreply.github.com>
2020-02-09 17:53:11 +0800

    Minor change

--- readmazefeature/readmaze.py
+++ readmazefeature/readmaze.py
@@ -6,29 +6,25 @@ fileoutput = []
 import os
 
 def load_csv(f):
-    file = f
-    
-    if (file == ""):
+    if (f == ""):
         print("File not selected")
         print("Number of lines read = 0")
         return "File not selected"
-
     else:
-
         #Try Catch Block
         try:
-            with open(file,'r') as csvfile:
+            with open(f,'r') as csvfile:
                 csv_reader = csv.reader(csvfile)
                 row_count=sum(1 for row in csvfile)
                 print("Reading",file, "...")
                 print("Number of lines read:", row_count)
                 if len(fileoutput) >= 1:
                     fileoutput.clear()
-                    fileoutput.append(file)
-                    return "Success"
+                    fileoutput.append(f)
                 else:
-                    fileoutput.append(file)
-                    return "Success"
+                    fileoutput.append(f)
+                    
+                return "Success"
                 #Return back to menu 
                 menu()
         except FileNotFoundError:
